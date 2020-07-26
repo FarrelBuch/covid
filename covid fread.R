cb3c740933f5c4771ceaf7ed5af5f6dae95e4770
FarrelBuch <fjbuch@gmail.com>
2020-07-26 21:49:16 +0000

    just to check

--- covid fread.R
+++ covid fread.R
@@ -16,3 +16,5 @@ mydt[county == "Allegheny", new.past14.per100000 := 100000*new.past14day/pop.all
 
 ggplot(data = mydt , aes(x = date, y = new.past14.per100000)) + geom_point() + facet_wrap(vars(county))
 ggplot(data = mydt[county == "Allegheny"] , aes(x = date, y = new.past14day)) + geom_point()
+
+# something change just to change
