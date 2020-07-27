b884ce7da632706b0a6692d7a2f4d03f35b58fd8
FarrelBuch <fjbuch@gmail.com>
2020-07-27 18:50:13 -0400

    taking out the bumpf

--- covid fread.R
+++ covid fread.R
@@ -10,8 +10,3 @@ mydt <- mydt[county == "Allegheny"]
 mydt[, new.past14day:= c(rep(NA, times=14), diff(cases, lag = 14))]
 
 mydt[, ggplot( mapping = aes(x = date, y = new.past14day)) + geom_point() ]
-
-# change 2020-07-27 17:19 2020-07-27 17:24 2020-07-27 17:52 2020-07-27 18:18 #2020-07-27 18:22
-# 2020-07-27 18:25 just a trydad # and again 2020-07-27 18:45
-
-# 2020-07-27 18:47 and again
